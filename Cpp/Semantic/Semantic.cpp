30777641f0fe10d173b97e0e8f9e7bb9d24c755f
Gabidal <Quanfoxes@gmail.com>
2020-01-30 12:46:46 +0200

    Beta 1.1.69

--- Cpp/Semantic/Semantic.cpp
+++ Cpp/Semantic/Semantic.cpp
@@ -26,3 +26,32 @@ void Semantic::Operator_Breaker(Token *t)
 	Raw_Order.push_back(t);
 	Raw_Order.push_back(t->Childs.at(0));
 }
+
+void Semantic::Assembler()
+{
+	//this function assembles the insides 
+	//of the Raw_list of mathematical tokens
+	//a = b + [c * d] - e
+}
+
+void Semantic::Order_Finder()
+{
+	//for loop until you can find most importance token
+	int Base = 0;
+	int Current = 0;
+	for (int i = 0; i < Raw_Order.size(); i++)
+	{
+		if (Raw_Order.at(i)->Name == "*" || Raw_Order.at(i)->Name == "/")
+		{
+			Current = 1;
+		}
+		else if (Raw_Order.at(i)->Name == "+" || Raw_Order.at(i)->Name == "-")
+		{
+			Current = 0;
+		}
+		if (Current > Base)
+		{
+			Base = Current;
+		}
+	}
+}
--- H/Semantic/Semantic.h
+++ H/Semantic/Semantic.h
@@ -10,10 +10,13 @@ class Semantic
 	Token* Output;
 	Token* Input;
 	vector<Token*> Raw_Order;
+	int Tip_Of_Importance = 0;
 public:
 	void Factory();
 	void Right_Sided_Derivation_Solver(Token *t);
 	void Operator_Breaker(Token* t);
+	void Assembler();
+	void Order_Finder();
 	Semantic(Token *i, Token& o)
 	{
 		Input = i;
