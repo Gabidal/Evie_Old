52c3edb83f90522c36f899d315333a8f992f2f90
Gabidal <Quanfoxes@gmail.com>
2020-06-11 21:59:01 +0200

    aaaah

--- IO/test.asm
+++ IO/test.asm
@@ -1,17 +1,17 @@
-__Z6bananaP3int: 
+_Z6bananaP3int: 
 push ebp 
 mov ebp , esp 
 mov eax, dword 0
 leave 
 ret
  
-__Z6bananaP3intEND: 
+_Z6bananaP3intEND: 
 export main
 global main
 main: 
   _main: 
   push dword 1
-  call __Z6bananaP3int
+  call _Z6bananaP3int
   add esp, dword 4
   mov eax, dword 0
   ret
