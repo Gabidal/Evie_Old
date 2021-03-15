a39667520001570f1bc0432c45fbd9bd0385f7a4
Gabidal <Quanfoxes@gmail.com>
2021-03-15 17:46:53 +0200

    .

--- Tests/Cpp/Test_Back_End.cpp
+++ Tests/Cpp/Test_Back_End.cpp
@@ -178,7 +178,7 @@ void Back_End_Test::Init()
 		{{10}, "Tests/IO/Ptr.e"},
 		{{2}, "Tests/IO/Cast.e"},
 		{{-6}, "Tests/IO/Math.e"},
-		{{1}, "Tests/IO/Type.e"},
+		{{1}, "Tests/IO/Type.e"}, 
 		{{1}, "Tests/IO/Array.e"},
 		{{5}, "Tests/IO/Func.e"},
 		{{100}, "Tests/IO/Conditions.e"},
